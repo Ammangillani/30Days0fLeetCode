ba42c36accaded300222d5eed9d63dabed056c35
Amman Gillani <137705820+Ammangillani@users.noreply.github.com>
2024-03-21 06:15:55 +0500

    Time: 99 ms (49.59%), Space: 59.9 MB (32.67%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.js
@@ -0,0 +1,17 @@
+
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+var merge = function(intervals) {
+    intervals.sort((a, b) => a[0] - b[0]);
+    const merged = [];
+    let currentInterval = intervals[0];
+    for (let i = 1; i < intervals.length; i++) {
+        const [currentStart, currentEnd] = currentInterval;
+        const [nextStart, nextEnd] = intervals[i];
+        nextStart <= currentEnd ? currentInterval[1] = Math.max(currentEnd, nextEnd) : (merged.push(currentInterval), currentInterval = intervals[i]);
+    }
+    merged.push(currentInterval);
+    return merged;
+};
